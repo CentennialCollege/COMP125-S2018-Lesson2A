e61f90e037858649e8fe949832658d050e714684
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-05-16 12:08:04 -0400

    updated project

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,33 +2,71 @@
 
 // IIFE - Immediately Invoked Function Expression
 // anonymous self executing function
-(function(){
+(function () {
     "use strict"
+
+    // About Button Click event handler
+    function AboutButtonClick() {
+        console.log("About Button Clicked!");
+    }
+
+    function AboutButtonOver(event) {
+        event.currentTarget.style.opacity = 0.3;
+    }
+
+    function AboutButtonOut(event) {
+        event.currentTarget.style.opacity = 1.0;
+    }
+
+    function HomeContent() {
+        let AboutButton = document.getElementById("AboutButton");
+
+        AboutButton.textContent = "About";
+
+        // About Button event listener
+        AboutButton.addEventListener("click", AboutButtonClick);
+        AboutButton.addEventListener("mouseover", AboutButtonOver);
+        AboutButton.addEventListener("mouseout", AboutButtonOut);
+    }
+
+    function ReturnPI() {
+        return Math.PI;
+    }
+
+    function AboutContent() {
+        let myNumber = ReturnPI();
+        console.log("My Funky Number: " + myNumber);
+
+        let paragraph = document.getElementById("paragraph");
+
+        let mySentence = "";
+
+        paragraph.textContent = mySentence;
+    }
 
     function Start() {
         // local variable
         let title = document.title;
 
-        switch(title) {
-            case "COMP125 - Home":
-                let AboutButton = document.getElementById("AboutButton");
-
-                AboutButton.addEventListener("click", function(){
-                    console.log("About Button was clicked!");
-                });
-            break;
-
-            case "COMP125 - About":
-            break;
-        }
-
-       
-
-
         console.log("App Started!");
         console.log("----------------------------");
         console.log("Title: " + title);
+
+        switch (title) {
+            case "COMP125 - Home":
+                HomeContent();
+                break;
+
+            case "COMP125 - About":
+                AboutContent();
+                break;
+
+            default:
+                break;
+        }
     }
 
-    window.onload = Start;
+    //window.onload = Start;
+
+    window.addEventListener("load", Start);
 })();
